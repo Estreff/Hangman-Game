2f26b1952597e2833607a15233a5b8f2b1a107b0
Erik Streff <Estreff@gmail.com>
2017-07-28 10:05:31 -0600

    Second Layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+// var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        // 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        // 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+  // create alphabet ul
+  // var buttons = function () {
+  //   myButtons = document.getElementById('buttons');
+  //   letters = document.createElement('ul');
+
+  //   for (var i = 0; i < alphabet.length; i++) {
+  //     letters.id = 'alphabet';
+  //     list = document.createElement('li');
+  //     list.id = 'letter';
+  //     list.innerHTML = alphabet[i];
+  //     check();
+  //     myButtons.appendChild(letters);
+  //     letters.appendChild(list);
+  //   }
+  // }
